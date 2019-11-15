a13a7598c6e8a6cb4de33d2c3d137d7910e2f7e8
melodyfff <307208327@qq.com>
2019-11-15 13:48:28 +0800

    :smile: 切换为多线程执行

--- py3/gitlab-api.py
+++ py3/gitlab-api.py
@@ -1,12 +1,18 @@
 # coding=utf-8
+
 # 提取gitlab的forks用户数量
+#
 # 依赖 ： pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple BeautifulSoup4
+#        pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple lxml
 import urllib.request
 from bs4 import BeautifulSoup
 import re
+import ssl
+import time
+from multiprocessing import Pool
 
 
-def url_open(url, time=20):  # 打开网页如： https://gitlab.com/fdroid/fdroidclient/-/forks?page=1
+def url_open(url, time_out=20):  # 打开网页如： https://gitlab.com/fdroid/fdroidclient/-/forks?page=1
     req = urllib.request.Request(url)
     req.add_header('User-Agent',
                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -23,9 +29,9 @@ def url_open(url, time=20):  # 打开网页如： https://gitlab.com/fdroid/fdro
                    '_gitlab_session=ed72896f8bd686e90b48e5f383f4f52a; _biz_nA=11; _biz_pendingA=%5B%5D; '
                    '_sp_id.6b85=943612f0-695c-4413-937b-ae3e2fdebf57.1545574812.8.1573733600.1546704064.dbd2e18e-cccf'
                    '-409d-895b-c6916a5cfe51111')
-    req.add_header('Host', 'gitlab.com')
+    req.add_header('Host', 'gitlab.cae.com')
 
-    response = urllib.request.urlopen(url, timeout=time)
+    response = urllib.request.urlopen(url, timeout=time_out, context=ssl._create_unverified_context())  # 忽略ssl认证
     html = response.read().decode("utf-8")
     return html
 
@@ -45,16 +51,19 @@ def total_page(html):  # 获取总页数
 
 
 def analysis_html(html):  # 解析页面，提取用户名
+    re_list = []
     soup = BeautifulSoup(html, 'lxml')
-    print("Title [%s]" % soup.title)
-    print("Title [%s]" % soup.title.string)
+    # print("Title [%s]" % soup.title)  # 打印 title
+    # print("Title [%s]" % soup.title.string) # 打印 title字符串https://gitlab.com/fdroid/fdroidclient
 
     names = soup.find_all('span', class_="namespace-name")
     for name in names:
+        re_list.append(name.string.replace("/", "").strip())
         print(name.string.replace("/", "").strip())  # 去除回车和空格
+    return re_list
 
 
-def get_total_page_list(html):
+def get_total_page_list(html):  # 获取访问页面列表
     pages = []
     total = total_page(html)
     while total > 0:
@@ -64,7 +73,40 @@ def get_total_page_list(html):
     return pages
 
 
-# save_html(url_open('https://gitlab.com/fdroid/fdroidclient/-/forks?page=1'))  # 打开网页并保存到本地
-total_page(open('hello.html', 'r', encoding='utf-8'))  # 统计总页数
-get_total_page_list(open('hello.html', 'r', encoding='utf-8'))  # 获取请求列表
-analysis_html(open('hello.html', 'r', encoding='utf-8'))  # 读取本地文件调试
+def test():  # 测试
+    save_html(url_open('https://gitlab.com/fdroid/fdroidclient/-/forks?page=1'))  # 打开网页并保存到本地
+    total_page(open('hello.html', 'r', encoding='utf-8'))  # 统计总页数
+    get_total_page_list(open('hello.html', 'r', encoding='utf-8'))  # 获取请求列表
+    analysis_html(open('hello.html', 'r', encoding='utf-8'))  # 读取本地文件调试
+
+
+def multi(page_list):  # 多线程的方式
+    p = Pool(10)
+    r = []
+    for page in page_list:
+        r.append(p.apply_async(func=multi_analysis, args=(page,)))
+    p.close()  # 关闭连接池,不再接收任务
+    p.join()  # 等待任务执行完毕
+
+    for res in r:  # 获取异步执行返回值
+        result.extend(res.get())
+
+
+def multi_analysis(url):
+    return analysis_html(url_open(url))
+
+
+def single(page_list):  # 单线程的方式
+    for i in page_list:
+        result.extend(analysis_html(url_open(i)))
+
+
+if __name__ == '__main__':
+    result = []
+    print("获取fork数开始")
+    start = time.time()
+    req_list = get_total_page_list(url_open('https://gitlab.com/fdroid/fdroidclient/-/forks?page=1'))  # 获取请求列表
+    multi(page_list=req_list)
+    print("Total Cost : [%f] s" % (time.time() - start))
+    print("Final Result: ", result)
+    print("Final Total: ", len(result))
